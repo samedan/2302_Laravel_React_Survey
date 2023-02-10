1f488633e103824c3b56c5520ff97138be2b4028
Daniel Popescu <same.dan@gmail.com>
2023-02-10 12:28:57 +0100

    Surveys answers & Questions

--- database/migrations/2023_02_10_110750_create_surveys_table.php
+++ database/migrations/2023_02_10_110750_create_surveys_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -15,7 +16,14 @@ return new class extends Migration
     {
         Schema::create('surveys', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->string('image', 255);
+            $table->string('title', 1000);
+            $table->string('slug', 1000);
+            $table->tinyInteger('status');
+            $table->text('description')->nullable();
             $table->timestamps();
+            $table->timestamp('expire_date')->nullable();
         });
     }
 
